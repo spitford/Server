0209d20ccc2b4427180918eaae780eec39da7cbf
spitford <boyleb@hotmail.com>
2019-12-12 16:03:58 -0500

    Adeed Classes for bullets

--- index.js
+++ index.js
@@ -2,11 +2,46 @@ var io = require('socket.io')(process.env.PORT || 52300);
 
 // Custom Classes
 var Player = require('./Classes/Player.js');
+var Bullet = require('./Classes/Bullet.js');
 
 console.log('Server Started');
 
 var players = [];
 var sockets = [];
+var bullets = [];
+
+setInterval(() => {
+    bullets.forEach(bullet => {
+        var isDestroyed = bullet.onUpdate();
+
+        if(isDestroyed) {
+            var index = bullets.indexOf(bullet);
+            if(index > -1) {
+                bullets.splice(index, 1);
+
+                var returnData = {
+                    id: bullet.id
+                }
+
+                for (var playerID in players) {
+                    sockets[playerID].emit('serverDespawn', returnData);
+                }
+            }
+        } else {
+            var returnData = {
+                id: bullet.id,
+                position: {
+                    x: bullet.position.x,
+                    y: bullet.position.y
+                }
+            }
+
+            for (var playerID in players) {
+                sockets[playerID].emit('updatePosition', returnData);
+            }
+        }
+    });
+}, 100, 0);
 
 io.on('connection', function(socket) {
     console.log('Client Connected');
@@ -43,6 +78,33 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('updateRotation', player);
     });
 
+    socket.on('fireBullet', function(data) {
+        var bullet = new Bullet();
+        bullet.name = 'Bullet';
+        bullet.position.x = data.position.x;
+        bullet.position.y = data.position.y;
+        bullet.direction.x = data.direction.x;
+        bullet.direction.y = data.direction.y;
+
+        bullets.push(bullet);
+
+        var returnData = {
+            name: bullet.name,
+            id: bullet.id,
+            position: {
+                x: bullet.position.x,
+                y: bullet.position.y
+            },
+            direction: {
+                x: bullet.direction.x,
+                y: bullet.direction.y
+            }
+        }
+
+        socket.emit('serverSpawn', returnData);
+        socket.broadcast.emit('serverSpawn', returnData);
+    });
+
     socket.on('disconnect', function() {
         console.log('Player Disconnected');
         delete players[thisPlayerID];
@@ -50,3 +112,21 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('disconnected', player);
     });
 });
+
+function interval(func, wait, times) {
+    var interv = function(w, t) {
+        return function() {
+            if (typeof t === "undefined" || t-- > 0) {
+                setTimeout(interv, w);
+                try {
+                    func.call(null);
+                } catch(e) {
+                    t = 0;
+                    throw e.toString();
+                }
+            }
+        };
+    }(wait, times);
+
+    setTimeout(interv, wait);
+}
