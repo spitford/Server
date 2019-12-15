16ad5266829599d4e18f8f27ad154b824226b0ef
spitford <boyleb@hotmail.com>
2019-12-15 14:11:44 -0500

    refactor and add lobbies

---
+++ Classes/Server.js
@@ -0,0 +1,96 @@
+let Connection = require('./Connection')
+let Player = require('./Player')
+
+let LobbyBase = require('./Lobbies/LobbyBase')
+let GameLobby = require('./Lobbies/GameLobby')
+let GameLobbySettings = require('./Lobbies/GameLobbySettings')
+
+module.exports = class Server {
+    constructor() {
+        this.connections = [];
+        this.lobbies = [];
+
+        this.lobbies[0] = new LobbyBase(0);
+    }
+
+    onUpdate() {
+        let server = this;
+
+        for(let id in server.lobbies) {
+            server.lobbies[id].onUpdate();
+        }
+    }
+
+    onConnected(socket) {
+        let server = this;
+        let connection = new Connection();
+        connection.socket = socket;
+        connection.player = new Player();
+        connection.server = server;
+
+        let player = connection.player;
+        let lobbies = server.lobbies;
+
+        console.log('Player connected (' + player.id + ')');
+        server.connections[player.id] = connection;
+
+        socket.join(player.lobby);
+        connection.lobby = lobbies[player.lobby];
+        connection.lobby.onEnterLobby(connection);
+
+        return connection;
+    }
+    
+    onDisconnected(connection = Connection) {
+        let server = this;
+        let id = connection.player.id;
+
+        delete server.connections[id];
+        console.log('Player ' + connection.player.displayPlayerInfo() + ' has disconnected');
+
+        connection.socket.broadcast.to(connection.player.lobby).emit('disconnected', {
+            id: id
+        });
+
+        server.lobbies[connection.player.lobby].onLeaveLobby(connection);
+    }
+
+    onAttemptToJoinGame(connection = Connection) {
+        let server = this;
+        let lobbyFound = false;
+
+        let gameLobbies = server.lobbies.filter(item => {
+            return item instanceof GameLobby;
+        });
+        console.log('Found (' + gameLobbies.length + ') lobbies on the server');
+
+        gameLobbies.forEach(lobby => {
+            if(!lobbyFound) {
+                let canJoin = lobby.canEnterLobby(connection);
+
+                if(canJoin) {
+                    lobbyFound = true;
+                    server.onSwitchLobby(connection, lobby.id);
+                }
+            }
+        });
+
+        if(!lobbyFound) {
+            console.log('Making a new Game Lobby');
+            let gamelobby = new GameLobby(gameLobbies.length + 1, new GameLobbySettings('FFA', 2));
+            server.lobbies.push(gamelobby);
+            server.onSwitchLobby(connection, gamelobby.id);
+        }
+    }
+
+    onSwitchLobby(connection = Connection, lobbyID) {
+        let server = this;
+        let lobbies = server.lobbies;
+
+        connection.socket.join(lobbyID);
+        connection.lobby = lobbies[lobbyID];
+
+        lobbies[connection.player.lobby].onLeaveLobby(connection);
+        lobbies[lobbyID].onEnterLobby(connection);
+    }
+}
--- index.js
+++ index.js
@@ -1,182 +1,18 @@
-var io = require('socket.io')(process.env.PORT || 52300);
-
-// Custom Classes
-var Player = require('./Classes/Player.js');
-var Bullet = require('./Classes/Bullet.js');
+let io = require('socket.io')(process.env.PORT || 52300);
+let Server = require('./Classes/Server.js')
 
 console.log('Server Started');
 
-var players = [];
-var sockets = [];
-var bullets = [];
+let server = new Server();
 
 setInterval(() => {
-    bullets.forEach(bullet => {
-        var isDestroyed = bullet.onUpdate();
-
-        if(isDestroyed) {
-            despawnBullet(bullet);
-        } else {
-            var returnData = {
-                id: bullet.id,
-                position: {
-                    x: bullet.position.x,
-                    y: bullet.position.y
-                }
-            }
-
-            for (var playerID in players) {
-                sockets[playerID].emit('updatePosition', returnData);
-            }
-        }
-    });
-
-    for(var playerID in players) {
-        let player = players[playerID];
-
-        if(player.isDead) {
-            let isRespawn = player.respawnCounter();
-
-            if(isRespawn) {
-                let returnData = {
-                    id: player.id,
-                    position: {
-                        x: player.position.x,
-                        y: player.position.y
-                    }
-                }
-
-                sockets[playerID].emit('playerRespawn', returnData);
-                sockets[playerID].broadcast.emit('playerRespawn', returnData);
-            }
-        }
-    }
+    server.onUpdate();
 }, 100, 0);
 
-function despawnBullet(bullet = Bullet) {
-    console.log('Destroying bullet (' + bullet.id + ')');
-    var index = bullets.indexOf(bullet);
-    if(index > -1) {
-        bullets.splice(index, 1);
-
-        var returnData = {
-            id: bullet.id
-        }
-
-        for (var playerID in players) {
-            sockets[playerID].emit('serverDespawn', returnData);
-        }
-    }
-}
-
 io.on('connection', function(socket) {
-    console.log('Client Connected');
-
-    var player = new Player();
-    var thisPlayerID = player.id;
-
-    players[thisPlayerID] = player;
-    sockets[thisPlayerID] = socket;
-
-    // Tell client ID
-    socket.emit('register', {id: thisPlayerID});
-    socket.emit('spawn', player);
-    socket.broadcast.emit('spawn', player);
-
-    // Get info on other players
-    for(var playerID in players) {
-        if(playerID != thisPlayerID) {
-            socket.emit('spawn', players[playerID]);
-        }
-    }
-
-    socket.on('updatePosition', function(data) {
-        player.position.x = data.position.x;
-        player.position.y = data.position.y;
-
-        socket.broadcast.emit('updatePosition', player);
-    });
-
-    socket.on('updateRotation', function(data) {
-        player.tankRotation = data.tankRotation;
-        player.barrelRotation = data.barrelRotation;
-
-        socket.broadcast.emit('updateRotation', player);
-    });
-
-    socket.on('fireBullet', function(data) {
-        var bullet = new Bullet();
-        bullet.name = 'Bullet';
-        bullet.activator = data.activator;
-        bullet.position.x = data.position.x;
-        bullet.position.y = data.position.y;
-        bullet.direction.x = data.direction.x;
-        bullet.direction.y = data.direction.y;
-
-        bullets.push(bullet);
-
-        var returnData = {
-            name: bullet.name,
-            id: bullet.id,
-            activator: bullet.activator,
-            position: {
-                x: bullet.position.x,
-                y: bullet.position.y
-            },
-            direction: {
-                x: bullet.direction.x,
-                y: bullet.direction.y
-            }
-        }
-
-        socket.emit('serverSpawn', returnData);
-        socket.broadcast.emit('serverSpawn', returnData);
-    });
-
-    socket.on('collisionDestroy', function(data) {
-        //console.log('Collision with bullet id: ' + data.id);
-        let returnBullets = bullets.filter(bullet => {
-            return bullet.id == data.id
-        });
-
-        returnBullets.forEach(bullet => {
-            let playerHit = false;
-
-            for(var playerID in players) {
-                if(bullet.activator != playerID) {
-                    let player = players[playerID];
-                    let distance = bullet.position.Distance(player.position);
-
-                    if(distance < 0.65) {
-                        playerHit = true;
-                        let isDead = player.dealDamage(50);
-                        if (isDead) {
-                            console.log('Player with id: ' + player.id + ' has died');
-                            let returnData = {
-                                id: player.id
-                            }
-                            sockets[playerID].emit('playerDied', returnData);
-                            sockets[playerID].broadcast.emit('playerDied', returnData);
-                        } else {
-                            console.log('Player with id: ' + player.id + ' has (' + player.health + ') health left');
-                        }
-                        despawnBullet(bullet);
-                    }
-                }
-            }
-
-            if (!playerHit) {
-                bullet.isDestroyed = true;
-            }
-        });
-    });
-
-    socket.on('disconnect', function() {
-        console.log('Player Disconnected');
-        delete players[thisPlayerID];
-        delete socket[thisPlayerID];
-        socket.broadcast.emit('disconnected', player);
-    });
+    let connection = server.onConnected(socket);
+    connection.createEvents();
+    connection.socket.emit('register', {'id': connection.player.id});
 });
 
 function interval(func, wait, times) {
