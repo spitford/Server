bee6d1f59b8e2c4a2ed1f6640471f99dddb62ae8
spitford <boyleb@hotmail.com>
2019-12-16 12:01:38 -0500

    replaced socket.io package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // ws://127.0.0.1:52300/socket.io/?EIO=4&transport=websocket
 // ws://tuttankgameserver.herokuapp.com:80/socket.io/?EIO=4&transport=websocket
 
+let io = require('socket.io')(process.env.PORT || 52300);
 let Server = require('./Classes/Server.js')
 
 console.log('Server Started');
