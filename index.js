18a08fe0705252d6e99b8a07547228dca3cda3aa
spitford <boyleb@hotmail.com>
2019-12-16 00:19:33 -0500

    added server info

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let io = require('socket.io')(process.env.PORT || 52300);
+// ws://127.0.0.1:52300/socket.io/?EIO=4&transport=websocket
+// ws://tuttankgameserver.herokuapp.com:80/socket.io/?EIO=4&transport=websocket
+
 let Server = require('./Classes/Server.js')
 
 console.log('Server Started');
